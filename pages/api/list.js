97cb48b35b92a910eacd5b5aafdf6f953f8e2ba4
antecanis8 <42382878+antecanis8@users.noreply.github.com>
2025-03-03 22:03:54 +0000

    index.js2

--- pages/api/list.js
+++ pages/api/list.js
@@ -2,16 +2,21 @@ import * as store from 'app-store-scraper';
 
 // 定义有效的 collection 值
 const VALID_COLLECTIONS = [
-  'TOP_FREE_IOS',
-  'TOP_PAID_IOS',
+  'TOP_MAC',
+  'TOP_FREE_MAC',
+  'TOP_GROSSING_MAC',
+  'TOP_PAID_MAC',
   'NEW_IOS',
   'NEW_FREE_IOS',
   'NEW_PAID_IOS',
-  'TOP_FREE_GAMES_IOS',
-  'TOP_PAID_GAMES_IOS',
+  'TOP_FREE_IOS',
+  'TOP_FREE_IPAD',
   'TOP_GROSSING_IOS',
-  'GROSSING_FREE_IOS'
+  'TOP_GROSSING_IPAD',
+  'TOP_PAID_IOS',
+  'TOP_PAID_IPAD'
 ];
+
 
 export default async (req, res) => {
   if (req.method !== 'GET') {
